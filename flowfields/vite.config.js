e265031a21c7d18d9d4c516eb269537a493bb8f7
craftlinks <craftlinks@protonmail.com>
2025-07-21 21:52:11 +0200

    production bugs

--- flowfields/vite.config.js
+++ flowfields/vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
-  }
+  },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/examples',
+          dest: 'examples'
+        }
+      ]
+    })
+  ]
 }) 
